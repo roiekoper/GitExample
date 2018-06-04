f8aa0ebe68d14d5db7b92901cab2e88220088026
roiekoper <roiekoper@gmail.com>
2018-06-04 16:08:36 +0300

    Init

---
+++ cmake-build-debug/CMakeFiles/GitExample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GitExample.dir/main.cpp.o"
+  "GitExample.pdb"
+  "GitExample"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GitExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
